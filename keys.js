c9777edfc7e275523fa02bc118e7c9c8ead149b1
Cherise Knox <cheriseknox@Cherises-MacBook-Pro.local>
2017-09-12 22:11:26 -0700

    update app

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('This is loaded'); 
+
+// Get API credentials by following these steps:
+// Step One: https://apps.twitter.com/app/new
+// Step Two: Use http:// for all URLs
+// Step Three: Then go to "Keys and Access Tokens" to get credentials for below
+// Step Four: Then click the button below on that page to create an access token
+
+exports.twitterKeys = {
+	consumer_key: 'dgtzjhjMQ1QhOmTkRsDVVxs3r',
+	consumer_secret: 'gB6C8rpSiM6YX36GBK2WgZj8QbqGwXGItaauB4muJ0u469lCP3',
+	access_token_key: '160351429-gNbmgzoaZP2HS5y0TttSDq0MJBDu8G2YJj65lGMf',
+	access_token_secret: 'H4g0M08ywstUDriop6xuZ6asUdvBB5zM8n7tJiuixoMnR',
+}; 
